fcfd0f362db99a61be33019f70bac078b73b8316
wakhea <wassim.khene@polymtl.ca>
2022-01-16 20:02:10 -0500

    Add presale page skeleton

--- src/views/Presale/Presale.tsx
+++ src/views/Presale/Presale.tsx
@@ -1,7 +1,20 @@
-import { Paper, Grid, Typography } from "@material-ui/core";
+import { Paper, Grid, Typography, Button, Box } from "@material-ui/core";
+import { Metric, MetricCollection } from "../../components/Metric";
+import { useWeb3Context } from "src/hooks/web3Context";
+
 import "./presale.scss";
 
 const Presale = () => {
+  const { provider, address, connect } = useWeb3Context();
+
+  let modalButton = [];
+
+  modalButton.push(
+    <Button variant="contained" color="primary" className="connect-button" onClick={connect} key={1}>
+      Connect Wallet
+    </Button>,
+  );
+
   return (
     <div id="presale-view">
       <Paper className={`ohm-card`}>
@@ -11,6 +24,58 @@ const Presale = () => {
               <Typography variant="h5">Presale</Typography>
             </div>
           </Grid>
+          <Grid item>
+            <MetricCollection>
+              <Metric
+                className="plus-bought"
+                label={`Total PLUS Bought`}
+                metric={"0"}
+                //isLoading={stakingAPY ? false : true}
+              />
+              <Metric
+                className="plus-price"
+                label={`Plus Token Price`}
+                metric={"0.01 BNB"}
+                //isLoading={stakingTVL ? false : true}
+              />
+              <Metric
+                className="presale-end"
+                label={`Presale End`}
+                metric={"January 26 2022"}
+                //isLoading={currentIndex ? false : true}
+              />
+            </MetricCollection>
+          </Grid>
+          <div className="presale-area">
+            {!address ? (
+              <div className="presale-wallet-notification">
+                <div className="wallet-menu" id="wallet-menu">
+                  {modalButton}
+                </div>
+                <Typography variant="h6">Connect your wallet to access presale</Typography>
+              </div>
+            ) : (
+              <div className="presale-buy-area">
+                <Button variant="contained" color="primary" className="buy-button" onClick={connect} key={1}>
+                  Buy Token
+                </Button>
+                <Box className="presale-data">
+                  <div className="data-row">
+                    <Typography>PLUS balance</Typography>
+                    <Typography className="price-data">0 PLUS</Typography>
+                  </div>
+                  <div className="data-row">
+                    <Typography>Contribution</Typography>
+                    <Typography className="price-data">0 BNB</Typography>
+                  </div>
+                  <div className="data-row">
+                    <Typography>Max Contribution</Typography>
+                    <Typography className="price-data">2 BNB</Typography>
+                  </div>
+                </Box>
+              </div>
+            )}
+          </div>
         </Grid>
       </Paper>
     </div>
