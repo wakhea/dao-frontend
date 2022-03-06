da64cf56d938842a204a64a64b68dd1fdd9286e5
wakhea <wassim.khene@polymtl.ca>
2022-03-05 23:25:10 -0500

    Add footer draft

--- src/components/Landing/Footer/footer.style.js
+++ src/components/Landing/Footer/footer.style.js
@@ -1,38 +1,23 @@
 import styled from 'styled-components';
-import FooterImage from '../common/assets/image/footer-bg.png';
+import FooterPattern from '../common/assets/image/footer-pattern.png';
 import { themeGet } from '@styled-system/theme-get';
 
 const FooterWrapper = styled.footer`
   position: relative;
   overflow: hidden;
+  background-repeat: repeat;
+  background-size: 90px;
+  padding-top: 30px;
+  padding-bottom: 20px;
+  position: relative;
+  background-image: url(${FooterPattern});
+
   @media (min-width: 576px) {
-    padding-top: 170px;
-    margin-top: -150px;
-    &:before {
-      content: '';
-      position: absolute;
-      width: 104%;
-      padding-bottom: 104%;
-      border-top-right-radius: 11%;
-      top: 14%;
-      left: 0;
-      pointer-events: none;
-      background-color: rgba(18, 103, 230, 0.03);
-      transform: rotate(-6deg);
-      @media (max-width: 767px) {
-        padding-bottom: 150%;
-      }
-    }
+    
   }
 
   .footer_container {
-    background-repeat: no-repeat;
-    background-position: center 50px;
-    padding-top: 80px;
-    padding-bottom: 80px;
-    position: relative;
     @media (min-width: 576px) {
-      background-image: url(${FooterImage?.src});
     }
     @media (max-width: 990px) {
       padding-bottom: 20px;
