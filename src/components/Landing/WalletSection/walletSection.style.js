fdeb01ae8c2c7875143a4b237fbf4abd386402ca
wakhea <wassim.khene@polymtl.ca>
2022-03-06 22:59:45 -0500

    Set stakers reward responsiveness

--- src/components/Landing/WalletSection/walletSection.style.js
+++ src/components/Landing/WalletSection/walletSection.style.js
@@ -16,77 +16,108 @@ export const ContentWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: space-around;
+  text-align: right;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+  }
   .image {
-    width: 30%;
-    @media only screen and (max-width: 991px) {
-      width: 50%;
-    }
+    width: 40%;
     @media only screen and (max-width: 768px) {
-      width: 100%;
-      display: none;
+      width: 50%;
+      padding-top: 0;
     }
     @media only screen and (max-width: 480px) {
+      margin-bottom: 30px;
     }
     img {
-      width: 100%;
-      object-fit: contain;
+      max-width: 100%;
+      max-height: 250px;
+      object-fit: cover;
+      float: left;
+      @media only screen and (max-width: 768px) {
+        min-width: 300px;
+        width: 100%;
+      }
+      @media only screen and (max-width: 768px) {
+        min-width: 200px;
+        max-height: 100%;
+      }
     }
   }
   .content {
-    width: 50%;
+    width: 60%;
+      
     padding-right: 15px;
     border-right: ${themeGet('colors.border')};
     border-radius: 15px;
-    text-align: right;
     @media only screen and (max-width: 991px) {
       width: 50%;
     }
-    @media only screen and (max-width: 768px) {
-      width: 100%;
-      margin-bottom: 50px;
-      padding-left: 0px;
+    @media only screen and (max-width: 991px) {
+      width: 60%;
     }
+    @media only screen and (max-width: 768px) {
+      width: 75%;
+      margin-bottom: 50px;
+    }
+
     @media only screen and (max-width: 480px) {
-      margin-bottom: 0px;
+      width: 85%;
     }
     h2 {
       color: ${themeGet('colors.menu', '#13296C')};
       font-size: 42px;
       line-height: 1.2;
-      font-weight: 300;
+      font-weight: bold;
       letter-spacing: -0.025em;
       margin-bottom: 27px;
       @media only screen and (max-width: 1440px) {
-        margin-bottom: 15px;
         font-size: 38px;
+        margin-bottom: 15px;
+      }
+      @media only screen and (max-width: 991px) {
+        font-size: 34px;
       }
       @media only screen and (max-width: 768px) {
-        font-size: 40px;
+        font-size: 30px;
         max-width: 100%;
-        text-align: left;
+        text-align: right;
       }
       @media only screen and (max-width: 480px) {
-        font-size: 30px;
+        font-size: 24px;
       }
     }
-    .content-description {
-      float: right;
-    }
     p {
+      font-size: 20px;
+      float: right;
+      line-height: 28px;
       text-align: justify;
       direction: rtl;
-      font-size: 16px;
-      line-height: 28px;
       color: ${themeGet('colors.paragraph', '#13296C')};
-      max-width: 400px;
-      margin-bottom: 0;
+      @media only screen and (max-width: 991px) {
+        font-size: 18px;
+      }
       @media only screen and (max-width: 768px) {
         max-width: 100%;
-        text-align: left;
+        text-align: right;
+        font-size: 16px;
+      }
+    }
+    img {
+      margin-top: 50px;
+      object-fit: cover;
+      width: 100%;
+      @media only screen and (max-width: 1440px) {
+        margin-top: 30px;
+      }
+      @media only screen and (max-width: 1099px) {
+        margin-top: 50px;
       }
     }
   }
 `;
-
+/*
+text-align: justify;
+      direction: rtl;*/
 export default SectionWrapper;
