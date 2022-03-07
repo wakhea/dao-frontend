40fceee978639382bc41ad89389860ebb6f6f349
wakhea <wassim.khene@polymtl.ca>
2022-03-06 22:22:25 -0500

    Set treasury revenue responsiveness

--- src/components/Landing/Investment/investment.style.js
+++ src/components/Landing/Investment/investment.style.js
@@ -30,21 +30,29 @@ export const ContentWrapper = styled.div`
   align-items: center;
   justify-content: space-around;
   .image {
-    width: 30%;
+    width: 40%;
     @media only screen and (max-width: 991px) {
-      width: 50%;
+      width: 40%;
     }
     @media only screen and (max-width: 768px) {
-      width: 100%;
+      width: 50%;
       margin-bottom: 40px;
     }
     img {
-      width: 100%;
+      width: 65%;
       object-fit: cover;
+      float: right;
+      @media only screen and (max-width: 768px) {
+        min-width: 250px;
+        width: 100%;
+      }
+      @media only screen and (max-width: 768px) {
+        min-width: 150px;
+      }
     }
   }
   .content {
-    width: 50%;
+    width: 60%;
     padding-left: 15px;
     border-left: ${themeGet('colors.border')};
     border-radius: 15px;
@@ -52,12 +60,17 @@ export const ContentWrapper = styled.div`
       width: 60%;
     }
     @media only screen and (max-width: 768px) {
-      width: 100%;
+      width: 75%;
       margin-bottom: 50px;
+    }
+
+    @media only screen and (max-width: 480px) {
+      width: 85%;
     }
     h2 {
       color: ${themeGet('colors.menu', '#13296C')};
       font-size: 42px;
+      
       line-height: 1.2;
       font-weight: bold;
       letter-spacing: -0.025em;
@@ -66,22 +79,29 @@ export const ContentWrapper = styled.div`
       @media only screen and (max-width: 1440px) {
         font-size: 38px;
       }
+      @media only screen and (max-width: 991px) {
+        font-size: 34px;
+      }
       @media only screen and (max-width: 768px) {
         max-width: 100%;
-        text-align: center;
+        text-align: left;
+        font-size: 30px;
       }
       @media only screen and (max-width: 480px) {
-        font-size: 30px;
+        font-size: 24px;
       }
     }
     p {
-      font-size: 16px;
+      font-size: 20px;
       line-height: 28px;
       color: ${themeGet('colors.paragraph', '#13296C')};
-      max-width: 400px;
+      @media only screen and (max-width: 991px) {
+        font-size: 18px;
+      }
       @media only screen and (max-width: 768px) {
-        max-width: 100%;
-        text-align: center;
+        width: 100%;
+        text-align: left;
+        font-size: 16px;
       }
     }
   }
